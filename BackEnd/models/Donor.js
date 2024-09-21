6785b5b5c30b0a5f27e15a027334a405c88afd1a
Your Name <you@example.com>
2024-09-22 02:58:18 +0300

    update database schema for Donor

--- BackEnd/models/Donor.js
+++ BackEnd/models/Donor.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 const User = require('./User');
 
 const DonorSchema = new mongoose.Schema({
-  donations: [{
+  availableDonations: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Donation'
-  }]
+  }],
+  type: { 
+    type: String, 
+    required: true 
+  }
 });
 
 module.exports = User.discriminator('Donor', DonorSchema);
+module.exports = Donor;
