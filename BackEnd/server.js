d7f4405fce63ba966081df1eb0e84ecc4d90ca6c
MKOdeh2024 <217103@ppu.edu.ps>
2024-09-21 18:22:34 +0300

    minor update

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,6 +3,9 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
+dotenv.config();
+
+
 const app = express();
 const port = 3000;
 
@@ -10,7 +13,6 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-dotenv.config();
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
