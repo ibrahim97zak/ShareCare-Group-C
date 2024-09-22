34a30c6a8851a805df8d15d4475fee94c9cd85c1
Your Name <you@example.com>
2024-09-22 03:06:03 +0300

    update

--- BackEnd/dbConfig/db.js
+++ BackEnd/dbConfig/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://linaqurom:2001@sahem.uhod4.mongodb.net/');
+    await mongoose.connect('mongodb+srv://sahem:2024@sahem.uhod4.mongodb.net/');
     console.log('MongoDB connected successfully');
   } catch (err) {
     console.error(`Error: ${err.message}`);
