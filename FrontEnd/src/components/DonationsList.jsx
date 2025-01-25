07f3757ee018f9bb65b118e124406cf87563fc69
ibrahim97zak <121764716+ibrahim97zak@users.noreply.github.com>
2025-01-25 14:37:13 +0200

    Environment Variables

--- FrontEnd/src/components/DonationsList.jsx
+++ FrontEnd/src/components/DonationsList.jsx
@@ -20,7 +20,6 @@ const DonationsList = () => {
           // Update state with fetched donations
           setDonations(response.data);
           console.log("Environment Variables:", import.meta.env);
-          console.log("API URL:", ApiUrl);
 
         }
         catch(error){
