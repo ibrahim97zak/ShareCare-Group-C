3ad7a244acf350d79a7ed2f3deefa196327157c2
ibrahim97zak <121764716+ibrahim97zak@users.noreply.github.com>
2025-01-26 01:56:17 +0200

    fix-build-command-v3

--- server.js
+++ server.js
@@ -64,13 +64,8 @@ app.use(errorHandler);
 const staticPath = path.join(__dirname, './FrontEnd/dist');
 
 
-app.options("*", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.status(204).end();
-});
+app.options("*", cors());
+
 
 app.use(express.static(staticPath));
 
