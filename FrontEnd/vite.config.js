71e9cf0871816894ee033504b6d06290ceb8bbcf
ibrahim97zak <121764716+ibrahim97zak@users.noreply.github.com>
2025-01-26 01:31:09 +0200

    fix-build-command-v2

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()], // Add React plugin if you're using React
-  server: {
-    port: 5173, // Default port for Vite
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory for the build
   },
 });
